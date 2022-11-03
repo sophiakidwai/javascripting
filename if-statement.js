3df5f7d60db05292646e5a0a257a2b0ac794b2b4
sophia kidawai <sophia.kidwai@telus.com>
2022-11-03 11:43:49 -0700

    initial commit with all my work so far

---
+++ if-statement.js
@@ -0,0 +1,16 @@
+const fruit = 'orange'
+fruit.length
+if (fruit => 5){
+    console.log('The fruit name has more than five characters.')
+} else{
+    console.log('The fruit name has five characters or less.')
+}
+
+
+// const fruit = 'orange'
+//if (fruit.length > 5) {
+  //console.log('The fruit name has more than five characters.')
+//} else {
+  //console.log('The fruit name has five characters or less.')
+//}
+
---
+++ looping-through-arrays.js
@@ -0,0 +1,5 @@
+const pets = ['cat', 'dog', 'rat']
+for (let i = 0; pets.length; i++) {
+  pets[i] + 's'
+}
+console.log(pets)
